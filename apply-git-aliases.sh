0bff17b2997cdcd15aff84753e53b281e0707572
Tobias Marstaller <tobias.marstaller@gmail.com>
2019-03-20 16:23:28 +0100

    Apply git aliases globally

--- apply-git-aliases.sh
+++ apply-git-aliases.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-git config alias.ready "!$DIR/git-ready.sh"
-git config alias.fuckit "!git add -A && git commit --amend --no-edit && git push --force-with-lease"
+
+git config --global alias.ready "!$DIR/git-ready.sh"
+git config --global alias.fuckit "!git add -A && git commit --amend --no-edit && git push --force-with-lease"
