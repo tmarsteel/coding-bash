8a9aa45bbdc10572535f90f902f936ba0d21d659
Tobias Marstaller <tobias.marstaller@gmail.com>
2020-06-29 15:55:09 +0200

    git-qc: optionally rebase onto updated remote

--- git-qc.sh
+++ git-qc.sh
@@ -2,8 +2,20 @@
 
 set -e
 
-gitRootDir=$(git rev-parse --show-toplevel 2> /dev/null)
-currentBranchName=$(git name-rev --no-undefined --name-only HEAD)
+function askYesNo {
+	read -n 1 -r
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+	  echo "true"
+  else
+  	echo "false"
+  fi
+}
+
+gitRootDir="$(git rev-parse --show-toplevel 2> /dev/null)"
+currentBranchName="$(git name-rev --no-undefined --name-only HEAD)"
+upstream="$(git branch --points-at HEAD "--format=%(upstream)" "$currentBranchName")"
+localShaBefore="$(git rev-parse --verify -q HEAD)"
 
 prefix=
 if [[ "$currentBranchName" =~ ^[A-Za-z0-9]{2,}-[0-9]+$ ]]
@@ -21,12 +33,35 @@ git commit -m "$prefix$message"
 
 set +e
 
-upstream=$(git rev-parse @{u} 2> /dev/null)
-if [[ "$?" != 0 ]]
+if [[ "$upstream" == "" ]]
 then
 	echo "> git push --set-upstream origin $currentBranchName"
 	git push --set-upstream origin "$currentBranchName"
 else
-  echo "> git push --force-with-lease"
-	git push --force-with-lease
+  git fetch origin "$upstream"
+  upstreamSha="$(git rev-parse --verify -q "$upstream" 2> /dev/null)"
+  if [[ "$upstreamSha" != "$localShaBefore" ]]
+  then
+  	echo -e "\e[1;31mThe upstream has changed. These commits have been added/removed:\e[0m"
+  	echo "> git --no-pager log $localShaBefore..$upstreamSha"
+  	git --no-pager log "$localShaBefore..$upstreamSha"
+  	echo
+  	echo -e "\e[1;31mRebase onto these? [Y/n]\e[0m"
+  	doRebase=$(askYesNo)
+  	echo 
+  	if [[ "$doRebase" == "true" ]]
+  	then
+  		echo "> git rebase --onto $upstream"
+  		git rebase --onto "$upstream"
+
+  		unmerged="$(git ls-files -u)"
+  		if [[ "$unmerged" != "" ]]
+  		then
+  		  >&2 echo -e "\e[1;31mYou have unresolved conflicts!\e[0m"
+  		  exit 1
+  		fi
+  	fi
+  fi
+  echo "> git push --force-with-lease=$upstreamSha"
+	git push "--force-with-lease=$upstreamSha"
 fi
--- git-reword.sh
+++ git-reword.sh
@@ -5,9 +5,27 @@ set -e
 SELFDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}" )")"
 "$SELFDIR/git-dirty.sh"
 
+push=false
+branch=$(git rev-parse --short --abbrev-ref=loose HEAD 2>/dev/null)
+if [[ "$branch" != "HEAD" ]]
+then
+	upstream="$(git branch --points-at HEAD "--format=%(upstream)" "$branch")"
+  localSha="$(git rev-parse --verify -q --branch "$branch")"
+  remoteSha="$(git rev-parse --verify -q --branch "$upstream")"
+  if [[ "$localSha" == "$remoteSha" ]]
+  then
+  	push=true
+  fi
+fi
+
 message="`git log -1 --format=format:%B`"
 newMessageFile="`mktemp`"
 echo "$message" > "$newMessageFile"
 
 git commit --amend --edit "--file=$newMessageFile"
 rm $newMessageFile
+
+if [[ "$push" == "true" ]]
+then
+	git push --force-with-lease
+fi
